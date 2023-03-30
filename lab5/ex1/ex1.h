9320c2e89253dd6c37b7139129232559f8b62b3b
Jean Souto <jeansouto123@gmail.com>
2023-03-30 02:40:02 -0300

    Create ex1.h

---
+++ lab5/ex1/ex1.h
@@ -0,0 +1,6 @@
+typedef char* TAD_string;
+TAD_string criar (int n);
+void ler (TAD_string str);
+int tamanho (TAD_string str);
+int contarx (TAD_string str, char x);
+void printar (TAD_string str);
