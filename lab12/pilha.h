4a6fcfeba80d05b3e88b781f9e5e4241380734b5
Jean Souto <jeansouto123@gmail.com>
2023-05-24 17:52:27 -0300

    Create pilha.h

---
+++ lab12/pilha.h
@@ -0,0 +1,14 @@
+#define MAX 100
+
+typedef struct pilha Pilha;
+
+Pilha *criar();
+void limpar(Pilha *p);
+int push (Pilha *p, int it);
+int pop (Pilha *p, int *it);
+int consultar (Pilha *p, int *it);
+int tamanho(Pilha *p);
+int pilhaVazia(Pilha *p);
+int pilhaCheia(Pilha*p);
+void mostrar (Pilha *p);
+int comparar (Pilha *p1, Pilha *p2);
